0821d98388ef47efa6c811eeeaefd08cb56f47d9
Yuri Abramov <9539674+yurinka1@users.noreply.github.com>
2024-11-21 11:04:46 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Newtonsoft.Json;
+using NuGet.Frameworks;
+
+namespace ShortNameToLongName;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length < 1)
+        {
+            Console.WriteLine("Usage: dotnet run <shortTargetFramework>");
+            return;
+        }
+
+        string shortName = args[0];
+
+        try
+        {
+            NuGetFramework framework = NuGetFramework.Parse(shortName);
+            string json = JsonConvert.SerializeObject(new
+            {
+                framework.Framework,
+                framework.Version,
+                framework.Platform,
+                framework.PlatformVersion,
+                framework.HasPlatform,
+                framework.HasProfile,
+                framework.Profile,
+                framework.DotNetFrameworkName,
+                framework.DotNetPlatformName,
+                framework.IsPCL,
+                framework.IsPackageBased,
+                framework.AllFrameworkVersions,
+                framework.IsUnsupported,
+                framework.IsAgnostic,
+                framework.IsAny,
+                framework.IsSpecificFramework,
+                ShortName = shortName
+            }, Formatting.None);
+            Console.Write(json);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
